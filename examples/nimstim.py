fb1c3edda7e830834453f10ceb9ca6063c531829
RVoor <63646486+RVoor@users.noreply.github.com>
2021-04-07 11:43:51 +0200

    Feature matrix
    Fixes for Viola Jones + first part of the pipeline for nimstim faces.

---
+++ examples/nimstim.py
@@ -0,0 +1,146 @@
+from pathlib import Path
+import pandas as pd
+import numpy as np
+import re
+
+from protosc.preprocessing import GreyScale, ViolaJones, CutCircle
+from protosc.feature_extraction import FourierFeatures
+from protosc.io import ReadImage
+
+
+def create_csv(stim_data_dir, write=False):
+    """ Create dataframe with all image IDs and sex, emotion, and mouth position depicted on image"""
+
+    # Read all images
+    files_path = list(stim_data_dir.glob('*'))
+    files_path = [x for x in files_path if x.suffix.lower() == ".bmp"]
+
+    files = list(x.stem for x in files_path)
+
+    # Create codebook with the meaning of all abbreviations
+    codebook = {0: {'female', 'male'},
+                1: {'calm', 'angry', 'happy', 'fear', 'sad', 'surprised', 'neutral', 'disgust'},
+                2: {'open', 'closed', 'exuberant'}}
+    output = [[], [], []]
+
+    # Decode sex, emotion, and mouth position depicted on image
+    for file in files:
+        file_parts = file.split('_')
+        file_parts[0] = file_parts[0][-1]
+
+        for i in range(0, 3):
+            try:
+                code = re.findall(r'(?<=\')' + file_parts[i].lower() + r'\w*', str(codebook[i]))[0]
+                output[i].append(code)
+            except:
+                if file_parts[i].lower() == 'sp':
+                    output[i].append('surprised')
+                elif file_parts[i].lower() == 'x':
+                    output[i].append('exuberant')
+                else:
+                    pass
+
+    # Create dataframe with all collected info about the image
+    df = {'file': files_path,
+          'sex': output[0],
+          'emotion': output[1],
+          'mouth': output[2]}
+    df = pd.DataFrame(df)
+    df.index.name = 'picture_id'
+
+    # Write dataframe to csv file
+    if write:
+        df.to_csv('overview.csv', index=True)
+
+    return df
+
+
+def select_files(stim_data_dir, select:str, write=False):
+    """ Put specified files through the pipeline"""
+
+    overview = create_csv(stim_data_dir, write=write)
+    variations = overview[select].unique()
+
+    overview['class'] = ''
+    for classification, variation in enumerate(variations):
+        index = overview[overview[select] == variation].index
+        overview.loc[index, 'class'] = classification
+
+    x = np.array(overview['class'])
+    files = np.array(overview['file'])
+
+    return files, x
+
+
+def feature_matrix(output, pipe_complex):
+    """ Create Fourier Matrix: input = pipeline output, row = image, column = Feature """
+
+    # If you only run 1 pipeline
+    if not isinstance(output[0], dict):
+        for image in range(len(output)):
+            data = np.array([image, str(pipe_complex), output[image]])
+
+            try:
+                final = np.append(final, data, axis=0)
+            except NameError:
+                final = data
+
+        final = np.array_split(final, int(len(final) / 3))
+
+    # If you run a pipe_complex
+    else:
+        output_array = np.array([])
+        for image in range(len(output)):
+            for pipe in output[image].keys():
+                data = np.array([image, pipe, output[image][pipe]])
+
+                try:
+                    output_array = np.append([output_array], [data], axis=0)
+                except ValueError:
+                    output_array = data
+            try:
+                final = np.append(final, output_array, axis=0)
+            except NameError:
+                final = output_array
+
+    # Give preview of output array in form of dataframe
+    preview = pd.DataFrame(final)
+    preview.rename(columns={0: 'image_id', 1: 'pipeline', 2: 'features'}, inplace=True)
+    print(preview)
+
+    return final
+
+
+def execute(pipe_complex, stim_data_dir: Path, select: str, write=False):
+    """ Execute functions with corresponding pipeline"""
+
+    # Put selected images through pipeline (e.g., classified on mouth (O, C, X))
+    print('Filtering images...')
+    files, x = select_files(stim_data_dir, select=select, write=write)
+
+    print('Putting images through pipeline...')
+    output = pipe_complex.execute(files)
+
+    # Create feature matrix from pipeline output
+    print('Creating feature matrix...')
+    feature_array = feature_matrix(output, pipe_complex)
+
+    return feature_array, x, files
+
+
+def main():
+
+    # Define pipeline
+    pipe1 = ReadImage() * ViolaJones(20) * CutCircle() * FourierFeatures()
+    pipe2 = ReadImage() * GreyScale() * ViolaJones(20) * CutCircle() * FourierFeatures()
+    pipe_complex = pipe1 + pipe2
+
+    # Define path to images
+    stim_data_dir = Path("..", "data", "Nimstim faces")
+
+    # Execute pipeline
+    execute(pipe_complex, stim_data_dir, select='mouth', write=False)
+
+
+if __name__ == "__main__":
+    main()
