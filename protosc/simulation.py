d44f642de04e4d60433b8c8216c16002e4c9c4ba
Roos Voorvaart <r.voorvaart@uu.nl>
2021-08-09 10:48:37 +0200

    rebase wrapper branch

--- protosc/simulation.py
+++ protosc/simulation.py
@@ -26,7 +26,10 @@ def create_simulation_data(n_features=400, n_examples=500, n_true_features=25,
     for i_feature, feature_idx in enumerate(selected_features):
         feature_matrix[ones, feature_idx] += biases[i_feature]
 
-    ground_truth = {"selected_features": selected_features, "biases": biases}
+    final_biases = np.zeros(n_features)
+    final_biases[selected_features] = biases
+    ground_truth = {"selected_features": selected_features,
+                    "biases": final_biases}
     return FeatureMatrix(feature_matrix), y, ground_truth
 
 
