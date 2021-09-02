6c5dadcc0fb7d869a6d93b3731f5903396f07fbc
Raoul Schram <r.d.schram@uu.nl>
2021-09-02 15:49:59 +0200

    Fix hog plotting

--- protosc/feature_extraction/hog.py
+++ protosc/feature_extraction/hog.py
@@ -32,31 +32,60 @@ class HOGFeatures(BasePipeElement):
             hog_cellsize=self.hog_cellsize)
 
     def _get_ref_func(self, img):
-        def ref_func():
-            # preallocate hog reference frame
-            ref_grid_hog = np.zeros(
-                [np.int(np.floor(img.shape[0]/self.hog_cellsize[0])),
-                 np.int(np.floor(img.shape[1]/self.hog_cellsize[1])),
-                 self.orientations])
-            c = 0
-            for x in range(0, ref_grid_hog.shape[1]):
-                for y in range(0, ref_grid_hog.shape[0]):
-                    for z in range(0, ref_grid_hog.shape[2]):
-                        ref_grid_hog[y, x, z] = c
-                        c = c+1
-            return ref_grid_hog
-        return str(self.__class__)+str(img.shape), ref_func
+        name = str(self.__class__)+str(img.shape)
+        ref_func = hog_ref_func
+        ref_kwargs = {
+            "img_shape": img.shape,
+            "hog_cellsize": self.hog_cellsize,
+            "orientations": self.orientations,
+        }
+        return name, ref_func, ref_kwargs
+
+#     def _get_ref_func(self, img):
+#         def ref_func():
+#             # preallocate hog reference frame
+#             ref_grid_hog = np.zeros(
+#                 [np.int(np.floor(img.shape[0]/self.hog_cellsize[0])),
+#                  np.int(np.floor(img.shape[1]/self.hog_cellsize[1])),
+#                  self.orientations])
+#             c = 0
+#             for x in range(0, ref_grid_hog.shape[1]):
+#                 for y in range(0, ref_grid_hog.shape[0]):
+#                     for z in range(0, ref_grid_hog.shape[2]):
+#                         ref_grid_hog[y, x, z] = c
+#                         c = c+1
+#             return ref_grid_hog
+#         return str(self.__class__)+str(img.shape), ref_func
 
     @property
     def _plot_func(self):
-        def plot(ref_grid, i_feature):
-            data = np.zeros(ref_grid.shape[:2])
-            for i in i_feature:
-                x, y, _ = np.where(ref_grid == i)
-                data[x[0], y[0]] += 1/ref_grid.shape[2]
-            plt.imshow(data, cmap="binary", vmin=0, vmax=1)
-            plt.show()
-        return plot
+        return hog_plot
+#         def plot(ref_grid, i_feature):
+
+#         return plot
+
+
+def hog_plot(ref_grid, i_feature):
+    data = np.zeros(ref_grid.shape[:2])
+    for i in i_feature:
+        x, y, _ = np.where(ref_grid == i)
+        data[x[0], y[0]] += 1/ref_grid.shape[2]
+    plt.imshow(data, cmap="binary", vmin=0, vmax=1)
+    plt.show()
+
+
+def hog_ref_func(img_shape, hog_cellsize, orientations):
+    ref_grid_hog = np.zeros(
+        [np.int(np.floor(img_shape[0]/hog_cellsize[0])),
+         np.int(np.floor(img_shape[1]/hog_cellsize[1])),
+         orientations])
+    c = 0
+    for x in range(0, ref_grid_hog.shape[1]):
+        for y in range(0, ref_grid_hog.shape[0]):
+            for z in range(0, ref_grid_hog.shape[2]):
+                ref_grid_hog[y, x, z] = c
+                c = c+1
+    return ref_grid_hog
 
 
 def hog_features(img, orientations=9, hog_cellsize=[10, 10]):
