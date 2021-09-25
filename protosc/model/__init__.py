9bacb2c445b769dd75ca5b5e267dcf84f41fa96d
Raoul Schram <r.d.schram@uu.nl>
2021-09-25 13:23:06 +0200

    Fix flake8 errors

--- protosc/model/__init__.py
+++ protosc/model/__init__.py
@@ -1,5 +1,8 @@
 from .filter import FilterModel
 from .final_selection import final_selection
-from protosc.model import genetic
+from .genetic import genetic_algorithm
 from .pseudo_random import PseudoRandomModel
 from .random import RandomModel
+
+__all__ = ["FilterModel", "final_selection", "genetic_algorithm",
+           "PseudoRandomModel", "RandomModel"]
